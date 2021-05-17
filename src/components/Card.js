ca3a71d101ec3004b01904af69df88f375f7fa56
Clerissa <clerissalewis@gmail.com>
2021-05-17 23:56:42 +0400

    Fixed imports structure

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import { CardActionArea, Container } from '@material-ui/core'
-import FormLabel from '@material-ui/core/FormLabel'
-import FormControl from '@material-ui/core/FormControl'
-import FormGroup from '@material-ui/core/FormGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
+import {
+  CardActionArea,
+  Container,
+  Card,
+  CardContent,
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+} from '@material-ui/core'
 import Checkbox from '@material-ui/core/Checkbox'
-import { Handbag } from 'phosphor-react'
 
 const useStyles = makeStyles((theme) => ({
   root: {
