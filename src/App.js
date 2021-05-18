059e60918bf7924eb86e13a93f1a8c3d4af8b36b
Rithik Makhija <rithikmakhija2012@gmail.com>
2021-05-18 15:29:25 +0400

    Made dashboard poGGERS

--- src/App.js
+++ src/App.js
@@ -5,44 +5,46 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import Login from './components/Login'
 import AdminHome from './components/AdminHome'
+import Dashboard from './components/Dashboard'
 import Homepage from './components/Homepage'
 import { AuthProvider } from './auth/auth'
 import PrivateRoute from './components/PrivateRoute'
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)')
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        typography: {
-          fontWeight: 400,
-          letterSpacing: 1,
-          textTransform: 'none',
-          button: {
-            fontWeight: 400,
-            letterSpacing: 1,
-            textTransform: 'none',
-          },
-        },
-        palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode]
-  )
-  return (
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <Router>
-          <div>
-            <Route exact path='/' component={Login} />
-            <PrivateRoute exact path='/admin' component={AdminHome} />
-            <PrivateRoute exact path='/home' component={Homepage} />
-          </div>
-        </Router>
-      </AuthProvider>
-    </ThemeProvider>
-  )
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)')
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                typography: {
+                    fontWeight: 400,
+                    letterSpacing: 1,
+                    textTransform: 'none',
+                    button: {
+                        fontWeight: 400,
+                        letterSpacing: 1,
+                        textTransform: 'none',
+                    },
+                },
+                palette: {
+                    type: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode]
+    )
+    return (
+        <ThemeProvider theme={theme}>
+            <AuthProvider>
+                <Router>
+                    <div>
+                        <Route exact path='/' component={Login} />
+                        <PrivateRoute exact path='/admin' component={AdminHome} />
+                        <PrivateRoute exact path='/home' component={Homepage} />
+                        <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                    </div>
+                </Router>
+            </AuthProvider>
+        </ThemeProvider>
+    )
 }
 
 export default App
