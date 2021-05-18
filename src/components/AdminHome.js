1f7b61ae25df1b3308a92475a1ffa26c4619574f
shyamr586 <v.shyamr586@gmail.com>
2021-05-18 07:46:24 +0400

    pulling changes

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { DataGrid } from '@material-ui/data-grid'
-<<<<<<< HEAD
-import TextField from '@material-ui/core/TextField'
-import Container from '@material-ui/core/Container'
-import { makeStyles } from '@material-ui/core/styles';
-import React, { useState, useEffect } from 'react'
-=======
 import { Typography, Container, TextField, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import app from '../auth/config'
 import { AuthContext } from '../auth/auth'
->>>>>>> 2e1d63c95d8b1796e78f855329e3c9584bbb680c
 
 const useStyles = makeStyles((theme) => ({
   theader: {
@@ -48,53 +41,6 @@ const AdminHome = (props) => {
   }, [setCustomers])
 
   return (
-<<<<<<< HEAD
-    <Container maxWidth='xs'>
-      <h1 className={classes.pagename}>Order Details</h1>
-      <form noValidate>
-        <TextField
-          style={{ overflow: 'hidden' }}
-          id='sdate'
-          label='Start Date'
-          type='date'
-          defaultValue='dd/m/yyyy'
-          // onChange={this.handleChange}
-          className={classes.textField}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField
-          style={{ overflow: 'hidden' }}
-          id='edate'
-          label='End Date'
-          type='date'
-          defaultValue='dd/m/yyyy'
-          // onChange={this.handleChange}
-          className={classes.textField}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </form>
-      <div style={{ height: 600, width: '100%', margin: '10px auto' }}>
-        {customers.item && (
-          <DataGrid
-            getRowId={(r) => r._id}
-            rows={customers.item}
-            columns={columns}
-            pageSize={9}
-            checkboxSelection
-            autoHeight
-            // onCellEnter =
-            //cler, uncomment the next two lines and check design and keep/remove (refresh need)
-            // showCellRightBorder = "true"
-            // showColumnRightBorder = "true"
-          />
-        )}
-      </div>
-    </Container>
-=======
     <>
       {currentUser.email.includes('admin') ? (
         <Container maxWidth='xs'>
@@ -142,7 +88,6 @@ const AdminHome = (props) => {
         </Typography>
       )}
     </>
->>>>>>> 2e1d63c95d8b1796e78f855329e3c9584bbb680c
   )
 }
 
