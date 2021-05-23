35051e54ac45fd68aea0a80f2b9ca9dbdd192eb8
mythxn <mithain3@icloud.com>
2021-05-23 10:58:37 +0400

    dede

--- src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/login', (req, res, next) => {})
-
-router.get('/user', (req, res, next) => {})
-
-module.exports = router
